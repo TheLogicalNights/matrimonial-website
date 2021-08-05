5c13f64b08efac7c8c6ba4925ca2bc15d9aff32c
TheLogicalNights <swapniladhav560@gmail.com>
2021-08-06 00:03:06 +0530

    Added title

--- joinus.php
+++ joinus.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Material Design for Bootstrap</title>
+    <title>Maitri Janmojanmichi | Join Us</title>
+    <link rel="icon" href="./img/3.png" sizes="32X32" type="image/png">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
     <!-- Google Fonts Roboto -->
