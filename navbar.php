ff0f54be136198f3b9633e0eb756a6809c656163
TheLogicalNights <swapniladhav560@gmail.com>
2021-08-07 19:25:10 +0530

    Added new link to navbar

--- navbar.php
+++ navbar.php
@@ -30,9 +30,9 @@
     <div class="collapse navbar-collapse" id="navbarButtonsExample">
       <!-- Left links -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <a class="nav-link" href="#">Dashboard</a>
-        </li> -->
+        </li>
       </ul>
       <!-- Left links -->
 
