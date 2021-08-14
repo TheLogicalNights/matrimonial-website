64ccb3ca1a46850a71b0d2ea752a74eb0c14b34e
TheLogicalNights <swapniladhav560@gmail.com>
2021-08-14 20:43:18 +0530

    Added database config

---
+++ database.model.php
@@ -0,0 +1,8 @@
+<?php
+     $host = "localhost";
+     $dbname = "matrimony1";
+     $username = "root";
+     $password = "swapnil123";
+     // Create connection
+     $conn = mysqli_connect($host, $username, $password, $dbname);
+?>
