c897bf00f980332a83524d9dc3730b915b61ec5c
TheLogicalNights <swapniladhav560@gmail.com>
2021-08-14 16:50:52 +0530

    Added login users

--- model/user.model.php
+++ model/user.model.php
@@ -9,6 +9,7 @@
         public $username;
 	    public $email;
 	    public $password;
+        public $isset;
 
         public function __construct($db)
         {
@@ -26,7 +27,7 @@
 
         public function createUser()
         {
-            $query = "INSERT into ".$this->table_name."(userid,name,username,email,password) values(?,?,?,?,?)";
+            $query = "INSERT into ".$this->table_name."(userid,name,username,email,password,isset) values(?,?,?,?,?,?)";
             //prepare this query
             $stmt = $this->conn->prepare($query);
             $stmt->bindParam(1,$this->id);
@@ -34,6 +35,7 @@
             $stmt->bindParam(3,$this->username);
             $stmt->bindParam(4,$this->email);
             $stmt->bindParam(5,$this->password);
+            $stmt->bindParam(6,$this->isset);
             // execute query
             if($stmt->execute())
             {   
@@ -43,6 +45,17 @@
             {
                 return false;
             }
+        }
+
+        public function loginUsers()
+        {
+            $query = "SELECT * from ".$this->table_name." where email = ? and password = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt ->bindParam(1,$this->email);
+            $stmt ->bindParam(2,$this->password);
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            return $result;          
         }
     }
 ?>
