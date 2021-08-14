2dbbaf49836ff0309879b8e7f2c3d96d9de86ff1
TheLogicalNights <swapniladhav560@gmail.com>
2021-08-15 01:14:20 +0530

    Added address details

--- address.php
+++ address.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['userid']))
+    {
+        header("Location: joinus.php" );
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,6 +22,8 @@
     <link rel="stylesheet" href="css/mdb.min.css" />
     <!-- Custom styles -->
     <link rel="stylesheet" href="css/style.css" />
+    <!-- sweet alert  -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 
 <body class="body">
@@ -26,6 +35,18 @@
         ?>
         <!-- Navbar -->
         <main>
+        <?php
+            if(isset($_SESSION['alreadyentereddetails']))
+            {
+                echo '
+                <script>
+                    swal("Oops..!", "'.$_SESSION['alreadyentereddetails'].'", "error");
+                </script>
+                ';
+                unset($_SESSION['alreadyentereddetails']);
+            }
+
+        ?>
             <section class="login">
                 <div class="container py-3">
                     <div class="row">
@@ -37,35 +58,34 @@
                                 <div class="col-lg-7 mx-auto text-white">
                                     <h1 class="mt-3 mb-4 text-center">ADDRESS DETAILS</h1>
                                     <!-- form -->
-                                    <form>
-                                        <label class="form-label text-white mb-4 text-left">Current Address</label>
+                                    <form action="./code.php" method="POST" method="POST">
                                         <div class="form-outline mb-4">
-                                            <input type="text" id="cAddress1" class="form-control" required />
+                                            <input type="text" id="cAddress1" name="caddress1" class="form-control" required />
                                             <label class="form-label text-white" for="cAddress1">Address line 1</label>
                                         </div>
 
                                         <!-- Text input -->
                                         <div class="form-outline mb-4">
-                                            <input type="text" id="cAddress2" class="form-control" required />
+                                            <input type="text" id="cAddress2" name="caddress2" class="form-control" required />
                                             <label class="form-label text-white" for="cAddress2">Address line 2</label>
                                         </div>
 
                                         <!-- Text input -->
                                         <div class="form-outline mb-4">
-                                            <input type="text" id="cAddress3" class="form-control" required />
+                                            <input type="text" id="cAddress3" name="caddress3" class="form-control" required />
                                             <label class="form-label text-white" for="cAddress3">Address line 3</label>
                                         </div>
 
                                         <div class="row mb-4">
                                             <div class="col">
                                                 <div class="form-outline">
-                                                    <input type="text" id="cCountry" class="form-control" required />
+                                                    <input type="text" id="cCountry" name="ccountry" class="form-control" required />
                                                     <label class="form-label text-white" for="cCountry">Country</label>
                                                 </div>
                                             </div>
                                             <div class="col">
                                                 <div class="form-outline">
-                                                    <input type="text" id="cState" class="form-control" required />
+                                                    <input type="text" id="cState" name="cstate" class="form-control" required />
                                                     <label class="form-label text-white" for="cState">State</label>
                                                 </div>
                                             </div>
@@ -73,73 +93,19 @@
                                         <div class="row mb-4">
                                             <div class="col">
                                                 <div class="form-outline">
-                                                    <input type="text" id="cCity" class="form-control" required />
+                                                    <input type="text" id="cCity" name="ccity" class="form-control" required />
                                                     <label class="form-label text-white" for="cCity">City</label>
                                                 </div>
                                             </div>
                                             <div class="col">
                                                 <div class="form-outline">
-                                                    <input type="text" id="cZip" class="form-control" required />
+                                                    <input type="text" id="cZip" name="czip" class="form-control" required />
                                                     <label class="form-label text-white" for="cZip">Zip <Code></Code></label>
                                                 </div>
                                             </div>
                                         </div>
-                                        <label class="form-label text-white mb-4 text-left">Permanant Address</label>
-                                        <div class="form-outline mb-4">
-                                            <input class="form-check-input ms-1" type="checkbox" value="" id="flexCheckDefault" onclick="checkAddress()" />
-                                            <label class="form-check-label" for="flexCheckDefault">
-                                                Same as current address
-                                            </label>
-                                        </div>
-                                        <div id="permanantaddress">
-                                            <div class="form-outline mb-4">
-                                                <input type="text" id="pAddress1" class="form-control" required />
-                                                <label class="form-label text-white" for="pAddress1">Address line 1</label>
-                                            </div>
-
-                                            <!-- Text input -->
-                                            <div class="form-outline mb-4">
-                                                <input type="text" id="pAddress2" class="form-control" required />
-                                                <label class="form-label text-white" for="pAddress2">Address line 2</label>
-                                            </div>
-
-                                            <!-- Text input -->
-                                            <div class="form-outline mb-4">
-                                                <input type="text" id="pAddress3" class="form-control" required />
-                                                <label class="form-label text-white" for="pAddress3">Address line 3</label>
-                                            </div>
-
-                                            <div class="row mb-4">
-                                                <div class="col">
-                                                    <div class="form-outline">
-                                                        <input type="text" id="pCountry" class="form-control" required />
-                                                        <label class="form-label text-white" for="pCountry">Country</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="form-outline">
-                                                        <input type="text" id="pState" class="form-control" required />
-                                                        <label class="form-label text-white" for="pState">State</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row mb-4">
-                                                <div class="col">
-                                                    <div class="form-outline">
-                                                        <input type="text" id="pCity" class="form-control" required />
-                                                        <label class="form-label text-white" for="pCity">City</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="form-outline">
-                                                        <input type="text" id="pZip" class="form-control" required />
-                                                        <label class="form-label text-white" for="pZip">Zip <Code></Code></label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
                                         <!-- Submit button -->
-                                        <button type="submit" class="btn btn-primary btn-block mb-4">Next</button>
+                                        <button type="submit" name="addressinfo" class="btn btn-primary btn-block mb-4">Next</button>
                                     </form>
                                 </div>
                             </div>
