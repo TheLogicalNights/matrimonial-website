f3b63f173e896ec493fd05945de9a6d82b4cad3e
TheLogicalNights <swapniladhav560@gmail.com>
2021-09-11 17:54:21 +0530

    Updated

--- admin.php
+++ admin.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+if (!isset($_SESSION['admin'])) {
+    header("Location: adminlogin.php");
+}
+include './database.model.php';
+$query1 = "SELECT userid FROM profile";
+$all_profiles = mysqli_query($conn, $query1);
+
+$query2 = "SELECT userid FROM profile where isactive='1'";
+$active_profiles = mysqli_query($conn, $query2);
+
+$query3 = "SELECT userid FROM profile where isactive='0'";
+$deactive_profiles = mysqli_query($conn, $query3);
+$allusercounter = 0;
+$activeusercounter = 0;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,7 +40,7 @@
     <!--Main Navigation-->
     <header>
         <?php
-            include "./adminnavbar.php";
+        include "./adminnavbar.php";
         ?>
     </header>
     <!-- Main Navigation -->
@@ -69,12 +86,12 @@
                                         <span>Deactivated Users</span>
                                     </a>
                                 </li>
-                                <li>
+                                <!-- <li>
                                     <a href="#" class="nav-link nav-link px-3 sidebar-link" onclick="displayAddUsers()">
                                         <span class="me-2"><i class="fas fa-user-plus"></i></span>
                                         <span>Add New User</span>
                                     </a>
-                                </li>
+                                </li> -->
                             </ul>
                         </div>
                     </li>
@@ -84,11 +101,11 @@
                     <li>
                         <div class="text-muted small fw-bold text-uppercase px-3">Account</div>
                     </li>
-                    <li>
+                    <!-- <li>
                         <a href="#" class="nav-link px-3" onclick="displayChangePassword()"><span class="me-2"><i class="fas fa-key"></i></span><span>Change Password</span></a>
-                    </li>
+                    </li> -->
                     <li>
-                        <a href="#" class="nav-link px-3"><span class="me-2"><i class="fas fa-sign-out-alt"></i></span><span>Signout</span></a>
+                        <a href="./code.php?signout=1" class="nav-link px-3"><span class="me-2"><i class="fas fa-sign-out-alt"></i></span><span>Signout</span></a>
                     </li>
                 </ul>
             </nav>
@@ -107,63 +124,109 @@
             <div class="row mt-3">
                 <div class="col-md-3 mb-3">
                     <div class="card text-white bg-primary h-100">
-                        <div class="card-header">Header</div>
+                        <div class="card-header">
+                            <h3>Total Users</h3>
+                        </div>
                         <div class="card-body">
-                            <h5 class="card-title">Primary card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the
-                                card's content.
-                            </p>
+                            <h5 class="card-title"><?php echo mysqli_num_rows($all_profiles); ?></h5>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3 mb-3">
                     <div class="card text-white bg-success h-100">
-                        <div class="card-header">Header</div>
+                        <div class="card-header">
+                            <h3>Active Users</h3>
+                        </div>
                         <div class="card-body">
-                            <h5 class="card-title">Primary card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the
-                                card's content.
-                            </p>
+                            <h5 class="card-title"><?php echo mysqli_num_rows($active_profiles); ?></h5>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3 mb-3">
                     <div class="card text-white bg-danger h-100">
-                        <div class="card-header">Header</div>
+                        <div class="card-header">
+                            <h3>Deactive Users</h3>
+                        </div>
                         <div class="card-body">
-                            <h5 class="card-title">Primary card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the
-                                card's content.
-                            </p>
+                            <h5 class="card-title"><?php echo mysqli_num_rows($deactive_profiles); ?></h5>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     </main>
     <!-- Dashboard -->
     <!-- All users -->
     <main class="mt-5 pt-3" id="allusers" style="display: none;">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12 fw-bold fs-3">
-                    All Users
-                </div>
-            </div>
+            <table class='table mt-5' id="myTable2">
+                <thead>
+                    <tr>
+                        <th>Sr.No</th>
+                        <th scope='col'>Name</th>
+                        <th scope='col'>Phone No.</th>
+                    </tr>
+                </thead>
+                <?php
+                while ($allusers = mysqli_fetch_assoc($all_profiles)) {
+                    $userid = $allusers['userid'];
+                    $allusercounter++;
+                    $query4 = "SELECT first_name,last_name,phone_no from personal_info where userid = '$userid'";
+                    $personal_info = mysqli_query($conn, $query4);
+                    while ($personal = mysqli_fetch_assoc($personal_info)) {
+                ?>
+
+                        <tr>
+                            <td><?php echo $allusercounter; ?></td>
+                            <td><?php echo $personal['first_name'] . " " . $personal['last_name']; ?></td>
+                            <td><?php echo $personal['phone_no']; ?></td>
+                        </tr>
+
+                <?php
+                    }
+                }
+                ?>
+            </table>
         </div>
     </main>
     <!-- All users -->
     <!-- Acivated Users -->
     <main class="mt-5 pt-3" id="activeusers" style="display: none;">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12 fw-bold fs-3">
-                    Active Users
-                </div>
-            </div>
+            <table class='table mt-2' id="myTable2">
+                <thead>
+                    <tr>
+                        <th scope='col'>Sr.No</th>
+                        <th scope='col'>Name</th>
+                        <th scope='col'>  Action</th>
+                    </tr>
+                    <?php
+                    while ($activeusers = mysqli_fetch_assoc($active_profiles)) {
+                        $userid = $activeusers['userid'];
+                        $query5 = "SELECT first_name,last_name from personal_info where userid = '$userid'";
+                        $Active_users_info = mysqli_query($conn, $query5);
+                        $activeusercounter++;
+                        while ($personal = mysqli_fetch_assoc($Active_users_info)) {
+                    ?>
+
+                            <tr>
+                                <td><?php echo $activeusercounter; ?></td>
+                                <td><?php echo $personal['first_name'] . " " . $personal['last_name']; ?></td>
+                                <td>
+                                    <form action='#' method='POST'>
+                                        <!-- <input type='hidden' name='username' id='visit' value=<?php echo $row['uname']; ?>> -->
+                                        <button type='submit' name='deactivate' class='btn btn-sm btn-outline-danger'>Deactivate</button>
+                                    </form>
+                                </td>
+                            </tr>
+
+                    <?php
+                        }
+                    }
+                    ?>
+            </table>
+            </thead>
         </div>
     </main>
     <!-- Acivated Users -->
@@ -179,7 +242,7 @@
     </main>
     <!-- Deactivated Users -->
     <!-- Add new User -->
-    <main class="mt-5 pt-3" id="addusers" style="display: none;">
+    <!-- <main class="mt-5 pt-3" id="addusers" style="display: none;">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12 fw-bold fs-3">
@@ -187,10 +250,10 @@
                 </div>
             </div>
         </div>
-    </main>
+    </main> -->
     <!-- Add new User -->
     <!-- change password -->
-    <main class="mt-5 pt-3" id="changepassword" style="display: none;">
+    <!-- <main class="mt-5 pt-3" id="changepassword" style="display: none;">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12 fw-bold fs-3">
@@ -198,7 +261,7 @@
                 </div>
             </div>
         </div>
-    </main>
+    </main> -->
     <!-- change password -->
     <!-- main section -->
     <!-- MDB -->
@@ -207,6 +270,18 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Custom scripts -->
     <script type="text/javascript" src="js/change-main.js"></script>
+
+    <script src="cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css">
+
+    <script>
+        $(document).ready(function() {
+            $('#myTable2').DataTable({
+                responsive: true
+            });
+        });
+    </script>
 </body>
+
 
 </html>
